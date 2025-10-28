147164dd93d86c3ea5f36f0b0e3e16154cd09bb0
iwamoto1 <110207145+iwamoto1@users.noreply.github.com>
2025-10-28 11:28:06 +0900

    Update transformer.py

--- groundingdino/models/GroundingDINO/transformer.py
+++ groundingdino/models/GroundingDINO/transformer.py
@@ -298,6 +298,39 @@ class Transformer(nn.Module):
             )  # (bs, \sum{hw}, 4) unsigmoid
             topk = self.num_queries
 
+            if topk > topk_logits.shape[-1]:
+                missing =  topk-topk_logits.shape[-1]
+                num_repeats = missing//topk_logits.shape[-1]
+                if num_repeats>0:
+                    missing = missing - num_repeats*topk_logits.shape[-1]
+                    repeat_pad1 = topk_logits.clone().detach().repeat(1, num_repeats)
+                    repeat_pad = topk_logits.clone().detach()[:,-missing:]
+                    topk_logits = torch.cat([topk_logits, repeat_pad1, repeat_pad], dim=-1)
+            
+                    repeat_pad1 = output_proposals.clone().detach().repeat(1, num_repeats, 1)
+                    repeat_pad = output_proposals.clone().detach()[:,-missing:, :]
+                    output_proposals = torch.cat([output_proposals, repeat_pad1, repeat_pad], dim=-2)
+            
+                    repeat_pad1 = enc_outputs_coord_unselected.clone().detach().repeat(1, num_repeats, 1)
+                    repeat_pad = enc_outputs_coord_unselected.clone().detach()[:,-missing:, :]
+                    enc_outputs_coord_unselected = torch.cat([enc_outputs_coord_unselected, repeat_pad1, repeat_pad], dim=-2)
+            
+                    repeat_pad1 = output_memory.clone().detach().repeat(1, num_repeats, 1)
+                    repeat_pad = output_memory.clone().detach()[:,-missing:, :]
+                    output_memory = torch.cat([output_memory, repeat_pad1, repeat_pad], dim=-2)
+                else:
+                    repeat_pad = topk_logits.clone().detach()[:,-missing:]
+                    topk_logits = torch.cat([topk_logits, repeat_pad], dim=-1)
+            
+                    repeat_pad = output_proposals.clone().detach()[:,-missing:, :]
+                    output_proposals = torch.cat([output_proposals, repeat_pad], dim=-2)
+            
+                    repeat_pad = enc_outputs_coord_unselected.clone().detach()[:,-missing:, :]
+                    enc_outputs_coord_unselected = torch.cat([enc_outputs_coord_unselected, repeat_pad], dim=-2)
+            
+                    repeat_pad = output_memory.clone().detach()[:,-missing:, :]
+                    output_memory = torch.cat([output_memory, repeat_pad], dim=-2)
+            
             topk_proposals = torch.topk(topk_logits, topk, dim=1)[1]  # bs, nq
 
             # gather boxes
